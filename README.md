f07dde5263aa63f1f7640e4179becd8e7135c6c1
Carolina <cpprates18@gmail.com>
2019-02-04 11:56:23 -0500

    changing subdirectory

---
+++ README.md
@@ -0,0 +1,1 @@
+# mean-stack-issue-tracker
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,58 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {
+  MatToolbarModule, MatFormFieldModule, MatInputModule,
+  MatOptionModule, MatSelectModule, MatIconModule,
+  MatCardModule, MatTableModule, MatSnackBarModule, MatDividerModule, MatButtonModule
+} from '@angular/material';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './component/list/list.component';
+import { CreateComponent } from './component/create/create.component';
+import { EditComponent } from './component/edit/edit.component';
+
+import { IssueService } from './issue.service';
+
+const routes: Routes = [
+  { path: 'create', component: CreateComponent },
+  { path: 'edit/:id', component: EditComponent },
+  { path: 'list', component: ListComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDividerModule,
+    MatSnackBarModule
+  ],
+  providers: [IssueService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
